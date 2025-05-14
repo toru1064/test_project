9be5e77298dd9f30db3fb94853c99476cfb208c1
とーる <kan.metaru@gmail.com>
2025-05-15 00:45:51 +0900

    /form追加

--- flasker/main.py
+++ flasker/main.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     books = [
-        {'title': 'はらぺこリン',
-        'price': '1200',
-        'arrival_day': '2020/7/9'},
-        {'title': 'ハリーポッター',
-        'price': '2500',
-        'arrival_day': '2020/12/12'}
+        # {'title': 'はらぺこリン',
+        # 'price': '1200',
+        # 'arrival_day': '2020/7/9'},
+        # {'title': 'ハリーポッター',
+        # 'price': '2500',
+        # 'arrival_day': '2020/12/12'}
     ]
     return render_template("index.html",books=books)
 
+@app.route("/form")
+def form():
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
