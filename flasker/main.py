f12c90f31c409940ddce9b75f67936e9d7a83776
とーる <kan.metaru@gmail.com>
2025-05-14 20:40:34 +0900

    初回のコミット

---
+++ flasker/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    books = [
+        {'title': 'はらぺこリン',
+        'price': '1200',
+        'arrival_day': '2020/7/9'},
+        {'title': 'ハリーポッターハリーポッター',
+        'price': '2500',
+        'arrival_day': '2020/12/12'}
+    ]
+    return render_template("index.html",book=books)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
